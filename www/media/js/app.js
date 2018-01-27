7c19d7581d2101f3c1929f9780661125ab174c57
mooniiaaa <mo_oniaa@wp.pl>
2018-01-27 18:21:05 +0100

    no message

--- www/media/js/app.js
+++ www/media/js/app.js
@@ -1,18 +1,30 @@
 //inicjalizacja obiektu - doczytac !!!
-new Vue ({
+var appk = new Vue ({
     //element - odwolanie do id'ka w htmlu (drzewie dom)
     el: '#app',
     //dane  doczytać !!!
     data: {
-        coin_list: []
+        coin_list: [],
+        paginate: ['coin_list'],
+        hiso_prices: []
     },
     methods:{
         get_coins: function(){
             this.$http.get('https://min-api.cryptocompare.com/data/all/coinlist').then(function(response){
-                this.coin_list = response.data;
+                var tmp = [];
+                var resp = response.data.Data;
+                for(var elem in resp){
+                // this.coin_list = response.data.Data;
+                tmp.push(resp[elem]);
+            };
+            this.coin_list = tmp;
             }, function(error){
                 console.log(error.statusText);
             });
+        },
+
+        get_prices: function(name){
+            console.log(name + " test");
         }
         
     },
@@ -20,3 +32,11 @@ new Vue ({
         this.get_coins();
     }
 });
+
+
+//funkcja onclick
+var showtr = function(obj){
+    appk.get_prices(obj.id);
+    var hist = obj.getElementsByClassName("trhidden")[0];
+    hist.classList.toggle('trvisible');
+};
