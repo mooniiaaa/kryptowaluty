a9c2ac96b570964315cbb27032fea5f3955670eb
mooniiaaa <mo_oniaa@wp.pl>
2018-01-28 22:54:00 +0100

    no message

--- www/media/js/app.js
+++ www/media/js/app.js
@@ -22,29 +22,30 @@ var appk = new Vue ({
             });
         },
 
-        get_prices: function(name){
-            if (name in this.histo_prices){
-                return;
+        get_prices: function(name, event){
+			var caller = event.currentTarget;    //odwolanie do obiektu ktory wywolal fukcje
+            if (name in this.histo_prices){      // sprawdzenie czy nie ma juz tych danych
+				this.toggle_visibility(caller);  // wywolanie metody ktora ma za zadanie pokazac/ukryc dodatkowa tabelke
+			} else {
+				var url = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + name + '&tsym=USD&limit=7'  // generowanie linka do api
+				this.$http.get(url).then(function(response){    // wyslanie zapytania 
+					this.histo_prices[name] = response.data.Data;
+					this.$forceUpdate();                        // wymuszenie odswiezenia DOM
+					this.$nextTick(function(){                  // wyczekanie od nastepnego odswiezenia
+						this.toggle_visibility(caller)          // wywolanie metody ktora ma za zadanie pokazac/ukryc dodatkowa tabelke
+					});
+				}, function(error){
+					alert(error.statusText);
+				}); 
             }
-            var url = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + name + '&tsym=USD&limit=7'
-            this.$http.get(url).then(function(response){
-                this.histo_prices[name] = response.data.Data;
-                this.$forceUpdate();
-            }, function(error){
-                console.log(error.statusText);
-            });
-        }
-        
+        },
+		
+		toggle_visibility: function(obj){
+			var tmp = obj.getElementsByClassName("subtable")[0];  // wyszukanie w obiekcie dodatkowej tabelki
+			tmp.classList.toggle('trhidden');                     // usuniecie/dodanie klasy do tabelki
+		}
     },
     mounted: function () {
         this.get_coins();
     }
 });
-
-//funkcja onclick
-var showtr = function(obj){
-    
-    appk.get_prices(obj.id);
-    var hist = obj.getElementsByClassName("trhidden")[0];
-    hist.classList.toggle('trvisible');
-};
